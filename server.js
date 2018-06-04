9c93426a9f5aefd53646f8c156bc32e59dd65cfd
nyx-coder <kadamshailesh007@gmail.com>
2018-06-04 20:11:18 +0530

    Initial Commit

---
+++ server.js
@@ -0,0 +1,57 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+var app = express();
+
+hbs.registerPartials(__dirname + '/views/partials')
+app.set('view engine', 'hbs');
+
+app.use(express.static(__dirname + '/public'));
+
+
+
+app.use((req, res, next) => {
+    var now = new Date().toString();
+    var log = `${now} ${req.method} ${req.url}`;
+    console.log(log);
+    fs.appendFileSync('server.log', log + '\n');
+    next();
+});
+
+app.use((req, res, next) => {
+    res.render('maintenence.hbs');
+});
+hbs.registerHelper('getCurrentYear', () => {
+    return new Date().getFullYear();
+})
+
+hbs.registerHelper('screamit', (text) => {
+    return text.toUpperCase();
+})
+
+app.get('/', (req, res) => {
+    res.render('home.hbs', {
+        user: 'Shailesh',
+        pageTitle: 'Home Page',
+    });
+});
+
+
+
+
+app.get('/about', (req, res) => {
+    res.render('about.hbs', {
+        pageTitle: 'About Page',
+    });
+});
+
+app.get('/bad', (req, res) => {
+    res.send({
+        errorMessage: 'Error Message'
+    })
+});
+app.listen(3000, () => {
+    console.log('Server is up on 3000');
+
+});
